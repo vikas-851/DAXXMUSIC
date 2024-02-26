49f33ea05a216fe8e5a0c95cfde5012d1e987da8
VG-TEAM <152384469+VG-TEAM@users.noreply.github.com>
2024-02-26 22:02:27 +0530

    Update rank.py

--- DAXXMUSIC/plugins/tools/rank.py
+++ DAXXMUSIC/plugins/tools/rank.py
@@ -1,153 +1,116 @@
 from pyrogram import filters
 from pymongo import MongoClient
-from Natasha import app
+from DAXXMUSIC import app
 from config import MONGO_DB
 from pyrogram.types import *
 
 
-mongo_client = MongoClient(MONGO_DB)
+# Connect to MongoDB
+mongo_client = MongoClient("your_mongo_db_uri")
 db = mongo_client["natu_rankings"]
 collection = db["ranking"]
 
+# Store today's and user data
+today = {}
 user_data = {}
 
-today = {}
-
+# Default profile picture
 pic = "https://telegra.ph/file/895fb19771f4c6bfc0607.jpg"
 
 
-# ------------------- watcher ----------------------- #
-
+# Watcher functions
 @app.on_message(filters.group & filters.group, group=6)
 def today_watcher(_, message):
     chat_id = message.chat.id
     user_id = message.from_user.id
-    if chat_id in today and user_id in today[chat_id]:
-        today[chat_id][user_id]["total_messages"] += 1
-    else:
-        if chat_id not in today:
-            today[chat_id] = {}
-        if user_id not in today[chat_id]:
-            today[chat_id][user_id] = {"total_messages": 1}
-        else:
-            today[chat_id][user_id]["total_messages"] = 1
+
+    today.setdefault(chat_id, {}).setdefault(user_id, {"total_messages": 0})
+    today[chat_id][user_id]["total_messages"] += 1
 
 
 @app.on_message(filters.group & filters.group, group=11)
 def _watcher(_, message):
-    user_id = message.from_user.id    
+    user_id = message.from_user.id
     user_data.setdefault(user_id, {}).setdefault("total_messages", 0)
-    user_data[user_id]["total_messages"] += 1    
+    user_data[user_id]["total_messages"] += 1
     collection.update_one({"_id": user_id}, {"$inc": {"total_messages": 1}}, upsert=True)
 
 
-# ------------------- ranks ------------------ #
+# Ranks functions
+def get_top_users(chat_id, data, limit=10):
+    users_data = [(user_id, user_data["total_messages"]) for user_id, user_data in data[chat_id].items()]
+    sorted_users_data = sorted(users_data, key=lambda x: x[1], reverse=True)[:limit]
+    return sorted_users_data
+
+
+def get_user_link(user_id):
+    try:
+        user_name = app.get_users(user_id).first_name
+        return f"[{user_name}](https://t.me/{user_name})"
+    except:
+        return "Unknown"
+
+
+def generate_leaderboard_caption(title, data):
+    response = f"**📈 LEADERBOARD {title}**\n"
+    for idx, (user_id, total_messages) in enumerate(data, start=1):
+        user_link = get_user_link(user_id)
+        user_info = f"**{idx}**. {user_link} • {total_messages}\n"
+        response += user_info
+    return response
+
 
 @app.on_message(filters.command("today"))
-async def today_(_, message):
+async def today_leaderboard(_, message):
     chat_id = message.chat.id
     if chat_id in today:
-        users_data = [(user_id, user_data["total_messages"]) for user_id, user_data in today[chat_id].items()]
-        sorted_users_data = sorted(users_data, key=lambda x: x[1], reverse=True)[:10]
-        
+        sorted_users_data = get_top_users(chat_id, today)
+
         if sorted_users_data:
-            response = "**📈 LEADERBOARD TODAY**\n"
-            for idx, (user_id, total_messages) in enumerate(sorted_users_data, start=1):
-                try:
-                    user_name = (await app.get_users(user_id)).first_name
-                except:
-                    user_name = "Unknown"
-                user_info = f"**{idx}**. {user_name} • {total_messages}\n"
-                response += user_info
-            button = InlineKeyboardMarkup(
-                [[    
-                   InlineKeyboardButton("OVERALL", callback_data="overall"),
-                ]])
-            await message.reply_photo(photo=pic, caption=response, reply_markup=button)
+            response = generate_leaderboard_caption("TODAY", sorted_users_data)
+            button = InlineKeyboardMarkup([[InlineKeyboardButton("OVERALL", callback_data="overall")]])
+            await message.reply_photo(photo=pic, caption=response, reply_markup=button, parse_mode='markdown')
         else:
             await message.reply_text("No data available for today.")
     else:
         await message.reply_text("No data available for today.")
 
 
-
 @app.on_message(filters.command("ranking"))
-async def ranking(_, message):
+async def overall_leaderboard(_, message):
     top_members = collection.find().sort("total_messages", -1).limit(10)
-    
-    response = "**📈 LEADERBOARD**\n"
-    for idx, member in enumerate(top_members, start=1):
-        user_id = member["_id"]
-        total_messages = member["total_messages"]
-        try:
-            user_name = (await app.get_users(user_id)).first_name
-        except:
-            user_name = "Unknown"
-        
-        user_info = f"**{idx}**. {user_name} • {total_messages}\n"
-        response += user_info 
-    button = InlineKeyboardMarkup(
-            [[    
-               InlineKeyboardButton("TODAY", callback_data="today"),
-            ]])
-    await message.reply_photo(photo=pic, caption=response, reply_markup=button)
+    sorted_users_data = [(member["_id"], member["total_messages"]) for member in top_members]
+
+    response = generate_leaderboard_caption("OVERALL", sorted_users_data)
+    button = InlineKeyboardMarkup([[InlineKeyboardButton("TODAY", callback_data="today")]])
+    await message.reply_photo(photo=pic, caption=response, reply_markup=button, parse_mode='markdown')
 
 
-
-# -------------------- regex -------------------- # 
-
+# Callback query functions
 @app.on_callback_query(filters.regex("today"))
 async def today_rank(_, query):
     chat_id = query.message.chat.id
     if chat_id in today:
-        users_data = [(user_id, user_data["total_messages"]) for user_id, user_data in today[chat_id].items()]
-        sorted_users_data = sorted(users_data, key=lambda x: x[1], reverse=True)[:10]
-        
+        sorted_users_data = get_top_users(chat_id, today)
+
         if sorted_users_data:
-            response = "**📈 LEADERBOARD TODAY**\n"
-            for idx, (user_id, total_messages) in enumerate(sorted_users_data, start=1):
-                try:
-                    user_name = (await app.get_users(user_id)).first_name
-                except:
-                    user_name = "Unknown"
-                user_info = f"**{idx}**. {user_name} • {total_messages}\n"
-                response += user_info
-            button = InlineKeyboardMarkup(
-                [[    
-                   InlineKeyboardButton("OVERALL", callback_data="overall"),
-                ]])
-            await query.message.edit_text(response, reply_markup=button)
+            response = generate_leaderboard_caption("TODAY", sorted_users_data)
+            button = InlineKeyboardMarkup([[InlineKeyboardButton("OVERALL", callback_data="overall")]])
+            await query.message.edit_text(response, reply_markup=button, parse_mode='markdown')
         else:
             await query.answer("No data available for today.")
     else:
         await query.answer("No data available for today.")
 
 
-
 @app.on_callback_query(filters.regex("overall"))
 async def overall_rank(_, query):
     top_members = collection.find().sort("total_messages", -1).limit(10)
-    
-    response = "**📈 LEADERBOARD**\n"
-    for idx, member in enumerate(top_members, start=1):
-        user_id = member["_id"]
-        total_messages = member["total_messages"]
-        try:
-            user_name = (await app.get_users(user_id)).first_name
-        except:
-            user_name = "Unknown"
-        
-        user_info = f"**{idx}**. {user_name} • {total_messages}\n"
-        response += user_info 
-    button = InlineKeyboardMarkup(
-            [[    
-               InlineKeyboardButton("TODAY", callback_data="today"),
-            ]])
-    await query.message.edit_text(response, reply_markup=button)
+    sorted_users_data = [(member["_id"], member["total_messages"]) for member in top_members]
+
+    response = generate_leaderboard_caption("OVERALL", sorted_users_data)
+    button = InlineKeyboardMarkup([[InlineKeyboardButton("TODAY", callback_data="today")]])
+    await query.message.edit_text(response, reply_markup=button, parse_mode='markdown')
 
 
-
-
-    
-
-    
+# Run the app
