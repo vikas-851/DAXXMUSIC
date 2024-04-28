17aa8c77193c7d1ba1f79ad95c437cee0f9ffa86
vikas-851 <167636013+vikas-851@users.noreply.github.com>
2024-04-28 20:28:21 +0530

    Update start.py

--- DAXXMUSIC/utils/inline/start.py
+++ DAXXMUSIC/utils/inline/start.py
@@ -1,8 +1,9 @@
-from pyrogram.types import InlineKeyboardButton
+from pyrogram import Client
+from pyrogram.types import InlineKeyboardButton, CallbackQuery
+from pyrogram import filters
 
 import config
 from DAXXMUSIC import app
-
 
 def start_panel(_):
     buttons = [
@@ -14,7 +15,6 @@ def start_panel(_):
         ],
     ]
     return buttons
-
 
 def private_panel(_):
     buttons = [
@@ -30,10 +30,26 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
-            InlineKeyboardButton(text=_["S_B_6"], callback_data="waifu"),
-     
+            InlineKeyboardButton(text=_["S_B_6"], callback_data="waifu"),     
         ],
     ]
     return buttons
 
+@app.on_callback_query(filters.regex("waifu"))
+async def waifu_callback(client: Client, callback_query: CallbackQuery):
 
+    await callback_query.answer()
+
+    # Send the help information for the "waifu" functionality
+    help_text = """
+    /guess: To Guess character (only works in group)
+    /fav: Add Your fav
+    /trade: To trade Characters
+    /gift: Give any Character from Your Collection to another user.. (only works in groups)
+    /collection: To see Your Collection
+    /topgroups: See Top Groups.. Ppl Guesses Most in that Groups
+    /top: Too See Top Users
+    /ctop: Your ChatTop
+    /changetime: Change Character appear time (only works in Groups)
+    """
+    await callback_query.message.reply_text(help_text)
