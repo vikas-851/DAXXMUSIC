807b24aa507739a48cb8586337fa4d93b574d000
vikas-851 <167636013+vikas-851@users.noreply.github.com>
2024-04-28 19:59:58 +0530

    Update start.py

--- DAXXMUSIC/utils/inline/start.py
+++ DAXXMUSIC/utils/inline/start.py
@@ -1,26 +1,3 @@
-from pyrogram import Client
-from pyrogram.types import CallbackQuery
-
-# Define the callback query handler for "waifu"
-@app.on_callback_query(filters.regex("waifu"))
-async def waifu_callback(client: Client, callback_query: CallbackQuery):
-    # Respond to the callback query
-    await callback_query.answer()
-
-    # Send the help information for the "waifu" functionality
-    help_text = """
-    /guess: To Guess character (only works in group)
-    /fav: Add Your fav
-    /trade: To trade Characters
-    /gift: Give any Character from Your Collection to another user.. (only works in groups)
-    /collection: To see Your Collection
-    /topgroups: See Top Groups.. Ppl Guesses Most in that Groups
-    /top: Too See Top Users
-    /ctop: Your ChatTop
-    /changetime: Change Character appear time (only works in Groups)
-    """
-    await callback_query.message.reply_text(help_text)
-
 from pyrogram.types import InlineKeyboardButton
 
 import config
@@ -59,3 +36,25 @@ def private_panel(_):
     ]
     return buttons
 
+from pyrogram import Client
+from pyrogram.types import CallbackQuery
+
+# Define the callback query handler for "waifu"
+@app.on_callback_query(filters.regex("waifu"))
+async def waifu_callback(client: Client, callback_query: CallbackQuery):
+    # Respond to the callback query
+    await callback_query.answer()
+
+    # Send the help information for the "waifu" functionality
+    help_text = """
+    /guess: To Guess character (only works in group)
+    /fav: Add Your fav
+    /trade: To trade Characters
+    /gift: Give any Character from Your Collection to another user.. (only works in groups)
+    /collection: To see Your Collection
+    /topgroups: See Top Groups.. Ppl Guesses Most in that Groups
+    /top: Too See Top Users
+    /ctop: Your ChatTop
+    /changetime: Change Character appear time (only works in Groups)
+    """
+    await callback_query.message.reply_text(help_text)
